5123e10f093459b1e44b3de198c3d52774edacdb
PalaciosCaroline <caroline2807@yahoo.fr>
2022-12-30 23:19:56 +0100

    Installation du menu hamburger

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 
 
-const domContainerMenu = document.querySelector('#menu_container');
-ReactDOM.render(<Menu/>, domContainerMenu);
+const hamburger = document.getElementById('hamburger')
+const navUl = document.getElementById('nav_ul')
+const boxImg = document.querySelector('.box_photo')
+const wrapTitle = document.querySelector('.wrapTitle')
+const mainFrozzen = document.querySelector('.main_frozzen')
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle("open")
+    navUl.classList.toggle("slide")
+    mainFrozzen.classList.toggle("frozzen")
+
+})
+
