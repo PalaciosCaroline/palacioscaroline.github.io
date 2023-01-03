c8f299772f25e64ea3b3942782498ff78ff30650
PalaciosCaroline <caroline2807@yahoo.fr>
2023-01-03 17:53:42 +0100

    rendu

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const navUl = document.getElementById('nav_ul')
 const boxImg = document.querySelector('.box_photo')
 const wrapTitle = document.querySelector('.wrapTitle')
 const mainFrozzen = document.querySelector('.main_frozzen')
-const header = document.getElementById('header')
+const header = document.getElementById('header_fixed')
 const btnHome = document.getElementById('btn_home')
 const boiteCapacity = document.getElementById('boiteCapacity')
 
@@ -47,3 +47,27 @@ window.addEventListener('resize', ulResize)
 btnHome.addEventListener('click', () => {
     boiteCapacity.classList.toggle('isVisible')
 })
+
+navUl.addEventListener('click', () =>{
+    hamburger.classList.remove("open")
+    navUl.classList.remove("slide")
+    mainFrozzen.classList.remove("frozzen")
+    document.body.classList.remove("noscroll");
+
+})
+
+
+// function pagebar()
+// {
+//   var links=document.getElementById('navbar').getElementsByTagName("a");
+//   var current = location.href;
+//   for (var i=0; i < links.length; i++)
+//   {
+//    if(links[i].href == current)
+//    {
+//     links[i].style.color ='blue';
+//       links[i].href = "";
+      
+//    }
+//  }
+// } 
